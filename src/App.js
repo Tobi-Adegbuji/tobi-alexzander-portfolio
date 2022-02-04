056aac570cca2dca8e68c28534cba2d6356c35e8
tobi-adegbuji-moonpi <tobi.adegbuji@accenture.com>
2022-02-04 15:59:19 -0500

    added section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import Banner from './Banner';
 import Navbar from './Navbar'; 
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <div className='main-content'>
+      <Banner/>
+
+      </div>
     </div>
   );
 }
