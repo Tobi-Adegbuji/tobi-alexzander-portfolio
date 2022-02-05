feba79189af7e865e9e54bd3d81e7394d9b18e60
tobi-adegbuji-moonpi <tobi.adegbuji@accenture.com>
2022-02-05 02:54:07 -0500

    addded picture

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import './Banner.css'; 
+import bannerImage from './img/banner-image.png'
 
 function Banner() {
   return (
   <section className='banner'>
+      <div className="banner-info">
+        <h1>HIRE ME</h1>
+      </div>
+      <div className="banner-infographic">     
+        <img src={bannerImage} alt="" />
+      </div>
   </section>
   );
 }
