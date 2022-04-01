9b5be97a0eb39d0970aa47b35e656eec5d4393e9
Tobi Adegbuji <48140431+Tobi-Adegbuji@users.noreply.github.com>
2022-04-01 08:34:00 -0400

    Update Banner.jsx

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -7,12 +7,12 @@ function Banner() {
   return (
     <section className="banner">
       <div className="banner-info">
-        <div className="banner-info-header-top">CREATIVE</div>
+        <div className="banner-info-header-top">DECENTRALIZED</div>
         <h1 className="banner-info-header-bottom">TECHNOLOGIST</h1>
         <div className="banner-info-line"></div>
         <p>
-          A creative individual with knowledge in application development;
-          working with a range of technologies to provide an excellent user
+          A creative individual with knowledge in decentalzed application development;
+          working with a range of blockchain technologies to provide an excellent user
           experience.{" "}
         </p>
         <div className="banner-info-btn">
@@ -22,14 +22,13 @@ function Banner() {
           <h2>How Can I Help You</h2>
           <h1>GROW?</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
-            soluta corrupti saepe cum illum qui temporibus, maiores incidunt,
-            impedit in, quos at ex inventore! Incidunt nesciunt unde quis?
-            Voluptatem doloribus modi dolor debitis amet fugiat animi quos
-            explicabo perferendis! Officia maiores fugiat voluptatum sequi,
-            animi quia deleniti mollitia maxime quos cumque, voluptates
-            repudiandae. Perferendis magni inventore, commodi deserunt natus
-            recusandae.
+          Web3 is the next era of the internet. It enables user trust by utilizing the blockchain 
+          to create decentralized platforms. No longer will power (or your data) remain with centralized companies such as Facebook.
+          This presents a tremendous opportunity for entrepreneurs and startups to launch decentralized applications (dApps for short). 
+          Because web3 and the blockchain is still in its infant stages, it would be smart to begin taking action now. 
+          Ok, so where do I come in? I am an experienced developer with specialized knowledge in blockchain development (specifically with NFTs). 
+          My goal is to help build clients a successful dApp. I will hold your hand from the in-and-outs of smart contract creation 
+          all the way to the front end UI design. If you have any inquiries, feel free to contact me!
           </p>
         </div>
       </div>
